8a8ca26a46c2ac2e5fee4beb4e8f50470e94824e
shreya <shreyab.udupi@gmail.com>
2024-02-19 16:28:26 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,33 +1,60 @@
-
 import './App.css';
 import Home from './components/Home';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Task from './components/AddTask';
+import { BrowserRouter as Router, Route, Routes, useHistory } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import Task from './components/AddTask';
 
 function App() {
   const [taskList, setTaskList] = useState(() => {
-    /*storedTaskList contains the details from the browser's local storage with the key 'taskList'.(stored as string)*/
     const storedTaskList = localStorage.getItem('taskList');
-    /*if storedTaskList has values then the string will be converted into js object otherwise empty array will be returned*/
     return storedTaskList ? JSON.parse(storedTaskList) : [];
-  }); 
+  });
 
   const updateTaskList = (newTask) => {
-    const newTaskList = [...taskList, newTask]; /*creates a new array neTaskList and adds the newTask to the existing task*/
+    const newTaskList = [...taskList, newTask];
     setTaskList(newTaskList);
-    localStorage.setItem('taskList', JSON.stringify(newTaskList)); /*Details in the local storage will be updated(stored as string)*/
+    localStorage.setItem('taskList', JSON.stringify(newTaskList));
   };
 
-  useEffect (() => {
+  const clearAllTasks = () => {
+    setTaskList([]);
+    localStorage.removeItem('taskList');
+  };
+
+  const editTask = (index) => {
+    const taskToEdit = taskList[index];
+
+    // Display a prompt for editing the task directly
+    const editedDate = prompt('Edit Date:', taskToEdit.date);
+    const editedTaskText = prompt('Edit Task:', taskToEdit.task);
+
+    if (editedDate !== null && editedTaskText !== null) {
+      const updatedTaskList = [...taskList];
+      updatedTaskList[index] = { date: editedDate, task: editedTaskText };
+      setTaskList(updatedTaskList);
+      localStorage.setItem('taskList', JSON.stringify(updatedTaskList));
+    }
+  };
+
+  const deleteTask = (index) => {
+    const updatedTaskList = [...taskList];
+    updatedTaskList.splice(index, 1);
+    setTaskList(updatedTaskList);
+    localStorage.setItem('taskList', JSON.stringify(updatedTaskList));
+  };
+
+  useEffect(() => {
     localStorage.setItem('taskList', JSON.stringify(taskList));
-  }, [taskList]); /*the effect will re-run only when the value of taskList changes */
-  
+  }, [taskList]);
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home taskList={taskList}/>} />
-        <Route path='/task' element={<Task updateTaskList={updateTaskList}/>}/>
+        <Route
+          path="/"
+          element={<Home taskList={taskList} clearAllTasks={clearAllTasks} editTask={editTask} deleteTask={deleteTask} />}
+        />
+        <Route path='/task' element={<Task updateTaskList={updateTaskList} />} />
       </Routes>
     </Router>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,36 +1,49 @@
+// Home.js
+
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaEdit, FaTrash} from 'react-icons/fa';
 
-
-const Home = ({taskList}) => { {/*taskList to display the list of tasks added*/}
+const Home = ({ taskList, clearAllTasks, editTask, deleteTask }) => {
   return (
     <div className='container mt-3 d-flex flex-column align-items-center'>
-        <div className='mt-5'>
+      <div className='mt-5'>
         <Link to="/task">
-            <button type="button" className="btn btn-primary">Add Task</button>
+          <button type="button" className="btn btn-primary">Add Task</button>
         </Link>
-        </div>
-        {taskList.length > 0 && (
-          <div className="mt-5 text-md-center">
-          <table className='table table-info table-hover table-bordered' style={{ maxWidth: '400px', margin: 'auto',}}>
+        <button type="button" className="btn btn-danger ms-2" onClick={clearAllTasks}>Clear All</button>
+      </div>
+      {taskList.length > 0 && (
+        <div className="mt-5 text-md-center">
+          <table className='table table-info table-hover table-bordered' style={{ maxWidth: '600px', margin: 'auto' }}>
             <thead>
               <tr>
-                <th scope='col' className='border'  style={{backgroundColor: '#4f6ec9', color: 'white'}}>Date</th>
-                <th scope='col' className='border' style={{backgroundColor: '#4f6ec9', color: 'white'}}>Task</th>
+                <th scope='col' className='border' style={{ backgroundColor: '#4f6ec9', color: 'white' }}>Date</th>
+                <th scope='col' className='border' style={{ backgroundColor: '#4f6ec9', color: 'white' }}>Task</th>
+                <th scope='col' className='border' style={{ backgroundColor: '#4f6ec9', color: 'white' }}>Actions</th>
               </tr>
             </thead>
             <tbody>
-              {taskList.map((item, index) => ( /*For each item in the tasklist the below code will be applied*/
-                <tr key={index}>   {/*index is the current item in the array*/}
+              {taskList.map((item, index) => (
+                <tr key={index}>
                   <td>{item.date}</td>
                   <td>{item.task}</td>
+                  <td>
+                    <span className="mx-2" style={{ cursor: 'pointer' }} onClick={() => editTask(index)}>
+                      <i><FaEdit /></i>
+                    </span>
+                    <span className="mx-2" style={{ cursor: 'pointer' }} onClick={() => deleteTask(index)}>
+                      <i><FaTrash /></i>
+                    </span>
+                  </td>
                 </tr>
               ))}
             </tbody>
           </table>
         </div>
-        )}
+      )}
     </div>
-  )
+  );
 }
-export default Home
+
+export default Home;
